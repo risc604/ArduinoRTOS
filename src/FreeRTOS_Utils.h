b171622d53d5ed837478110f1c18648b93b33c26
Denis Benato <benato.denis96@gmail.com>
2017-06-28 09:45:50 +1100

    debugging macros

--- src/FreeRTOS_Utils.h
+++ src/FreeRTOS_Utils.h
@@ -3,13 +3,13 @@
 #include "boost/preprocessor.hpp"
 #include "boost/preprocessor/cat.hpp"
 
-#define aDefineStaticQueue(qName,elSize,elCount) \
-static uint8_t BOOST_PP_CAT(qName,_StaticBuffer) [elSize * elCount]; \
-static StaticQueue_t BOOST_PP_CAT(qName,_StaticQueue); \
-static QueueHandle_t BOOST_PP_CAT(qName,_QueueHandle);
+#define aDefineStaticQueue( qName , elSize , elCount ) \
+static uint8_t BOOST_PP_CAT(qName, _StaticBuffer)[elSize * elCount]; \
+static StaticQueue_t BOOST_PP_CAT(qName, _StaticQueue); \
+static QueueHandle_t BOOST_PP_CAT(qName, _QueueHandle);
 
 #define aGetStaticQueueName( qName ) \
-BOOST_PP_CAT(qName,_QueueHandle)
+BOOST_PP_CAT(qName, _QueueHandle)
 
-#define aInitStaticQueue( qName, elSize, elCount ) \
-aGetQueueName(qName) = xQueueCreateStatic( elCount, elSize , BOOST_PP_CAT(qName,_StaticBuffer) , & BOOST_PP_CAT(qName,_StaticQueue));
+#define aInitStaticQueue( qName , elSize , elCount ) \
+aGetQueueName( qName ) = xQueueCreateStatic( elCount , elSize , BOOST_PP_CAT(qName, _StaticBuffer) , & ( BOOST_PP_CAT(qName, _StaticQueue) );
