feaef79034a76ab90936384929a8f8b0c65b2971
Denis Benato <benato.denis96@gmail.com>
2017-06-28 09:29:36 +1100

    added another macro

--- src/FreeRTOS_Utils.h
+++ src/FreeRTOS_Utils.h
@@ -3,10 +3,13 @@
 #include "boost/preprocessor.hpp"
 #include "boost/preprocessor/cat.hpp"
 
-#define defineStaticQueue(qName,elSize,elCount) \
+#define aDefineStaticQueue(qName,elSize,elCount) \
 static uint8_t BOOST_PP_CAT(qName,_StaticBuffer) [elSize * elCount]; \
 static StaticQueue_t BOOST_PP_CAT(qName,_StaticQueue); \
 static QueueHandle_t BOOST_PP_CAT(qName,_QueueHandle);
 
-#define initializeStaticQueue( qName, elSize, elCount ) \
+#define aInitStaticQueue( qName, elSize, elCount ) \
 BOOST_PP_CAT(qName,_QueueHandle) = xQueueCreateStatic( elCount, elSize , BOOST_PP_CAT(qName,_StaticBuffer) , & BOOST_PP_CAT(qName,_StaticQueue));
+
+#define aGetQueue( qName ) \
+BOOST_PP_CAT(qName,_QueueHandle)
