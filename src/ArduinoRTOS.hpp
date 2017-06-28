bb9db696083848e538fe71b96a0375202b3a53e2
Denis Benato <benato.denis96@gmail.com>
2017-06-28 21:06:49 +1100

    Static process

--- src/ArduinoRTOS.hpp
+++ src/ArduinoRTOS.hpp
@@ -3,6 +3,8 @@
 #include "Arduino_FreeRTOS.h"
 #include "semphr.h"
 #include "queue.h"
+#include "task.h"
+#include "timers.h"
 
 // This one is mine. lol
 #include "FreeRTOS_Utils.h"
--- src/FreeRTOS_Utils.h
+++ src/FreeRTOS_Utils.h
@@ -2,6 +2,9 @@
 
 #include "boost/preprocessor.hpp"
 
+// -----------------------------------------------------------------------------
+// ------------------------- Static Queue --------------------------------------
+// -----------------------------------------------------------------------------
 #define aDefineStaticQueue(qName , elSize , elCount) \
 static uint8_t BOOST_PP_CAT(qName, _StaticBuffer)[elSize * elCount]; \
 static StaticQueue_t BOOST_PP_CAT(qName, _StaticQueue); \
@@ -14,3 +17,17 @@ extern QueueHandle_t BOOST_PP_CAT(qName, _QueueHandle);
 
 #define aInitStaticQueue(qName, elSize, elCount) \
 BOOST_PP_CAT(qName, _QueueHandle) = xQueueCreateStatic(elCount, elSize, BOOST_PP_CAT(qName, _StaticBuffer), & BOOST_PP_CAT(qName, _StaticQueue));
+
+// -----------------------------------------------------------------------------
+// -------------------------- Static Task --------------------------------------
+// -----------------------------------------------------------------------------
+
+#define aDefineStaticTask(tName, tStackSize) \
+static StaticTask_t BOOST_PP_CAT(tName, _StaticTask); \
+static TaskHandle_t BOOST_PP_CAT(tName, _TaskHandle); \
+static StackType_t BOOST_PP_CAT(tName, _StackSpace)[tStackSize]; \
+
+#define aGetStaticTaskName(tName) BOOST_PP_CAT(tName, _TaskHandle);
+
+#define aCreateTask(tName, tFunction, tParams, tPriority) \
+BOOST_PP_CAT(tName, _TaskHandle) = xTaskCreateStatic(tFunction, (const char *) #tName, sizeof(BOOST_PP_CAT(tName, _StackSpace)) / sizeof(StackType_t), (const void*)tParams, tPriority, BOOST_PP_CAT(tName, _StackSpace), &BOOST_PP_CAT(tName, _StaticTask));
